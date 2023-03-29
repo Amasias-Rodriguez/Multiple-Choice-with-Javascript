047e0f5737ba2e5554daf8cebf06d1f9428728bd
Amasías Rodríguez <100006082+Amasias-Rodriguez@users.noreply.github.com>
2023-03-29 00:01:17 -0600

    Javascript file

---
+++ index.js
@@ -0,0 +1,75 @@
+//select all elements
+const start = document.getElementById("start");
+const quiz = document.getElementById("quiz");
+const question = document.getElementById("question");
+const qImg = document.getElementById("qImg");
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById("B");
+const choiceC = document.getElementById("C");
+const counter = document.getElementById("counter");
+const timeGauge = document.getElementById("timeGauge");
+const progress = document.getElementById("progres");
+const scoreDiv = document.getElementById("score");
+
+//create our question
+
+let questions = [
+    {
+        question : "What does HTML stand for?",
+        imgSrc : "img/html.png",
+        choiceA : "Correct",
+        choiceB : "Wrong",
+        choiceC : "Wrong",
+        correct : "A"
+    },{
+        question : "What does CSS stand for?",
+        imgSrc : "img/css.png",
+        choiceA : "Wrong",
+        choiceB : "Correct",
+        choiceC : "Wrong",
+        correct : "B"
+    },{
+        question : "What does JS stand for?",
+        imgSrc : "img/js.png",
+        choiceA : "Wrong",
+        choiceB : "Wrong",
+        choiceC : "Correct",
+        correct : "C"
+    }
+];
+
+//create some variables
+
+const lastQuestion = questions.legnth - 1;
+let runningQuestion = 0;
+
+// render a question
+function renderQuestion(){
+    let q = questions[runningQuestion];
+
+    question.innerHTML = "<p>"+ q.question +"</p>";
+    qImg.innerHTML = "<img src="+ q.imgSrc +">";
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
+}
+
+start.style.display = "none";
+renderQuestion();
+quiz.style.display = "block";
+
+//render progress
+
+function renderProgress(){
+    for(let qIndex = 0; qIndex <= lastQuestion;
+        qIndex++){
+            progress.innerHTML += "<div class='prog' id="+ qIndex +"></div>";
+        }
+}
+
+// counter render
+
+let count 
+const questionTime = 10; // 10s
+const gaugeWidth  = 150; // 150px
+const gaugeUnit = gaugeWidth / questionTime; 
